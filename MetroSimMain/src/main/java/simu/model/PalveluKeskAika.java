6a3c7db4eeff937002904dfcff9983e8d7b1ac40
TheZink <ilkka.sinkonen@gmail.com>
2025-03-04 12:49:20 +0200

    Ilkka: Lisäsin Saap ja Metro luokkaan

--- MetroSimMain/src/main/java/simu/model/PalveluKeskAika.java
+++ MetroSimMain/src/main/java/simu/model/PalveluKeskAika.java
@@ -5,28 +5,50 @@ import java.util.HashMap;
 import simu.framework.Kello;
 
 public class PalveluKeskAika {
+    private HashMap<Integer, Double> Saap_saapumislista = new HashMap<Integer, Double>();
     private HashMap<Integer, Double> lippu_saapumislista = new HashMap<Integer, Double>();
     private HashMap<Integer, Double> lait_saapumislista = new HashMap<Integer, Double>();
+    private HashMap<Integer, Double> metro_saapumislista = new HashMap<Integer, Double>();
 
-    private static int lippuPalveltu = 0;       // Palveltujen asiakkaiden määrä
-    private static int laitPalveltu = 0;        
+    private static int saapPalveltu = 0;        // Palveltujen asiakkaiden määrä
+    private static int lippuPalveltu = 0;       
+    private static int laitPalveltu = 0;
+    private static int metroPalveltu = 0;        
 
-    private static double lippuKeskiaika = 0;   // Keskimääräinen käsittelyaika
+    private static double saapKeskiaika = 0;    // Keskimääräinen käsittelyaika
+    private static double lippuKeskiaika = 0;
     private static double laituriKeskiaika = 0;
+    private static double metroKeskiaika = 0;
 
-    // Metodi tallentaa asiakkaan saapumisajan ja id lippu_aikalistaan
+    // Metodit tallentavat asiakkaan saapumisajan palvelupisteelle ja id listaan
+    public void setSaapumis(int id) {
+        Double saapumisaika = Kello.getInstance().getAika();
+        Saap_saapumislista.put(id, saapumisaika);
+    }
+    
     public void setLippu(int id) {
         Double saapumisaika = Kello.getInstance().getAika();
         lippu_saapumislista.put(id, saapumisaika);
     }
 
-    // Metodi tallentaa asiakkaan saapumisajan ja id lait_aikalistaan
     public void setLaituri(int id) {
         Double saapumisaika = Kello.getInstance().getAika();
         lait_saapumislista.put(id, saapumisaika);
     }
 
-    // Metodi poimii asiakkaan saapumisajan ja laskee keskiarvon
+    public void setMetro(int id) {
+        Double saapumisaika = Kello.getInstance().getAika();
+        metro_saapumislista.put(id, saapumisaika);
+    }
+
+    // Metodit poimivat asiakkaan saapumisajat palvelupisteille ja laskevat keskiarvon
+    public void getSaapumis(int id) {
+        Double saapumisaika = Saap_saapumislista.get(id);
+        Double poistumisaika = Kello.getInstance().getAika();
+        saapKeskiaika += poistumisaika - saapumisaika;
+        saapPalveltu++;
+    }
+
     public void getLippu(int id) {
         Double saapumisaika = lippu_saapumislista.get(id);
         Double poistumisaika = Kello.getInstance().getAika();
@@ -34,7 +56,6 @@ public class PalveluKeskAika {
         lippuPalveltu++;
     }
 
-    // Metodi poimii asiakkaan saapumisajan ja laskee keskiarvon
     public void getLaituri(int id) {
         Double saapumisaika = lait_saapumislista.get(id);
         Double poistumisaika = Kello.getInstance().getAika();
@@ -42,14 +63,29 @@ public class PalveluKeskAika {
         laitPalveltu++;
     }
 
-    // Palauttaa lipunmyyntipisteen keskimääräisen käsittelyajan
+    public void getMetro(int id) {
+        Double saapumisaika = metro_saapumislista.get(id);
+        Double poistumisaika = Kello.getInstance().getAika();
+        metroKeskiaika += poistumisaika - saapumisaika;
+        metroPalveltu++;
+    }
+
+    // Metodit palauttavat palvelupisteiden keskimääräisen käsittelyajan
+
+    public double getSaapKeskiaika() {
+        return saapKeskiaika / saapPalveltu;
+    }
+
     public double getLippuKeskiaika() {
         return lippuKeskiaika / lippuPalveltu;
     }
 
-    // Palauttaa laiturin keskimääräisen käsittelyajan
     public double getLaituriKeskiaika(){
         return laituriKeskiaika / laitPalveltu;
+    }
+
+    public double getMetroKeskiaika() {
+        return metroKeskiaika / metroPalveltu;
     }
 }
 
