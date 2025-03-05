a292ced6e2e97915e2c2d4e72ec5bee0aea6900c
TheZink <ilkka.sinkonen@gmail.com>
2025-03-05 12:53:43 +0200

    Ilkka: SQL yhteys

--- MetroSimMain/src/main/java/datasource/DbConnection.java
+++ MetroSimMain/src/main/java/datasource/DbConnection.java
@@ -1,5 +1,35 @@
 package datasource;
 
-public class DbConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DbConnection {
+private static Connection connection = null;
+
+    public static Connection getConnection(){
+        if(connection == null){
+            try {
+                connection = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/Metrosimulaattori?user=Metrosimulaattori&password=MetroSimulaattori");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Connection: " + connection);
+        return connection;
+    }
+
+    public static boolean isConnected(){
+        return connection != null;
+    }
+
+    public static void closeConnection(){
+        if(connection != null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
