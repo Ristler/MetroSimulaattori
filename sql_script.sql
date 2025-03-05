44623a321310e27443ea3e3ba6b20704458442fa
TheZink <ilkka.sinkonen@gmail.com>
2025-03-05 14:12:45 +0200

    Muokkasin scriptin

--- sql_script.sql
+++ sql_script.sql
@@ -3,39 +3,40 @@ CREATE DATABASE MetroSimulaattori;
 
 DROP USER IF EXISTS 'MetroSimulaattori'@'localhost';
 CREATE USER 'MetroSimulaattori'@'localhost' IDENTIFIED BY 'MetroSimulaattori';
+
 GRANT ALL PRIVILEGES ON MetroSimulaattori.* TO 'MetroSimulaattori'@'localhost';
 
 USE MetroSimulaattori;
 
 CREATE TABLE Metroasema (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    pvm DATE,
+    pvm DATE DEFAULT CURRENT_DATE,
     Asiak_palveltu INT DEFAULT 0,
-    Keskim_jonotusaika DOUBLE DEFAULT 0,
-    Simulointiaika DOUBLE DEFAULT 0
+    Keskim_jonotusaika FLOAT(5,2) DEFAULT 0,
+    Simulointiaika FLOAT(10,2) DEFAULT 0
 );
 
 CREATE TABLE Lippuhalli (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    pvm DATE,
+    pvm DATE DEFAULT CURRENT_DATE,
     Asiak_palveltu INT DEFAULT 0,
-    Keskim_jonotusaika DOUBLE DEFAULT 0,
-    Simulointiaika DOUBLE DEFAULT 0
+    Keskim_jonotusaika FLOAT(5,2) DEFAULT 0,
+    Simulointiaika FLOAT(10,2) DEFAULT 0
 );
 
 CREATE TABLE Laituri (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    pvm DATE,
+    pvm DATE DEFAULT CURRENT_DATE,
     Asiak_palveltu INT DEFAULT 0,
-    Keskim_jonotusaika DOUBLE DEFAULT 0,
-    Simulointiaika DOUBLE DEFAULT 0
+    Keskim_jonotusaika FLOAT(5,2) DEFAULT 0,
+    Simulointiaika FLOAT(10,2) DEFAULT 0
 );
 
 CREATE TABLE Metro (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    pvm DATE,
+    pvm DATE DEFAULT CURRENT_DATE,
     Asiak_palveltu INT DEFAULT 0,
-    Keskim_jonotusaika DOUBLE DEFAULT 0,
-    Simulointiaika DOUBLE DEFAULT 0
+    Keskim_jonotusaika FLOAT(5,2) DEFAULT 0,
+    Simulointiaika FLOAT(10,2) DEFAULT 0
 );
 
