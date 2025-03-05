138eb4f7c43835c2ff170a8e27b12d6a7a5174a3
TheZink <ilkka.sinkonen@gmail.com>
2025-03-05 11:22:39 +0200

    Loin SQL scriptin

---
+++ sql_script.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS MetroSimulaattori;
+CREATE DATABASE MetroSimulaattori;
+
+DROP USER IF EXISTS 'MetroSimulaattori'@'localhost';
+CREATE USER 'MetroSimulaattori'@'localhost' IDENTIFIED BY 'MetroSimulaattori';
+GRANT ALL PRIVILEGES ON MetroSimulaattori.* TO 'MetroSimulaattori'@'localhost';
+
+USE MetroSimulaattori;
+
+CREATE TABLE Metroasema (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pvm DATE,
+    Asiak_palveltu INT DEFAULT 0,
+    Keskim_jonotusaika DOUBLE DEFAULT 0,
+    Simulointiaika DOUBLE DEFAULT 0
+);
+
+CREATE TABLE Lippuhalli (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pvm DATE,
+    Asiak_palveltu INT DEFAULT 0,
+    Keskim_jonotusaika DOUBLE DEFAULT 0,
+    Simulointiaika DOUBLE DEFAULT 0
+);
+
+CREATE TABLE Laituri (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pvm DATE,
+    Asiak_palveltu INT DEFAULT 0,
+    Keskim_jonotusaika DOUBLE DEFAULT 0,
+    Simulointiaika DOUBLE DEFAULT 0
+);
+
+CREATE TABLE Metro (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pvm DATE,
+    Asiak_palveltu INT DEFAULT 0,
+    Keskim_jonotusaika DOUBLE DEFAULT 0,
+    Simulointiaika DOUBLE DEFAULT 0
+);
+
