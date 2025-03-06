4bef0477de3657bb7f0e73da6887c62716179df7
TheZink <ilkka.sinkonen@gmail.com>
2025-03-06 13:08:13 +0200

    Ilkka: Lisäsin uuden queryn

--- MetroSimMain/src/main/java/dao/MetroDao.java
+++ MetroSimMain/src/main/java/dao/MetroDao.java
@@ -4,6 +4,9 @@ import datasource.DbConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 
 public class MetroDao {
 
@@ -21,6 +24,32 @@ public class MetroDao {
             e.printStackTrace();
         }
 
+    }
+
+    public ArrayList<String> getData(String taulu) {
+        ArrayList<String> data = new ArrayList<>();
+        Connection connection = DbConnection.getConnection();
+        String sql = "SELECT * FROM " + taulu + "ORDER BY id DESC LIMIT 1";
+        
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                data.add(Integer.toString(rs.getInt("id")));
+                data.add(rs.getDate("pvm").toString());
+                data.add(rs.getTime("Kello").toString());
+                data.add(Integer.toString(rs.getInt("Asiak_palveltu")));
+                data.add(Float.toString(rs.getFloat("Keskim_jonotusaika")));
+                data.add(Float.toString(rs.getFloat("Simulointiaika")));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+        
     }
 
 }
