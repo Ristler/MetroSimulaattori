dce0162caf9e5435b0dbfa2d020065c46b55df18
TheZink <ilkka.sinkonen@gmail.com>
2025-03-06 10:25:44 +0200

    Ilkka: Loin asiakas-taulun debuggausta varten

--- sql_script.sql
+++ sql_script.sql
@@ -11,6 +11,7 @@ USE MetroSimulaattori;
 CREATE TABLE Metroasema (
     id INT PRIMARY KEY AUTO_INCREMENT,
     pvm DATE DEFAULT CURRENT_DATE,
+    kello TIME DEFAULT CURRENT_TIME,
     Asiak_palveltu INT DEFAULT 0,
     Keskim_jonotusaika FLOAT(5,2) DEFAULT 0,
     Simulointiaika FLOAT(10,2) DEFAULT 0
@@ -19,6 +20,7 @@ CREATE TABLE Metroasema (
 CREATE TABLE Lippuhalli (
     id INT PRIMARY KEY AUTO_INCREMENT,
     pvm DATE DEFAULT CURRENT_DATE,
+    kello TIME DEFAULT CURRENT_TIME,
     Asiak_palveltu INT DEFAULT 0,
     Keskim_jonotusaika FLOAT(5,2) DEFAULT 0,
     Simulointiaika FLOAT(10,2) DEFAULT 0
@@ -27,6 +29,7 @@ CREATE TABLE Lippuhalli (
 CREATE TABLE Laituri (
     id INT PRIMARY KEY AUTO_INCREMENT,
     pvm DATE DEFAULT CURRENT_DATE,
+    kello TIME DEFAULT CURRENT_TIME,
     Asiak_palveltu INT DEFAULT 0,
     Keskim_jonotusaika FLOAT(5,2) DEFAULT 0,
     Simulointiaika FLOAT(10,2) DEFAULT 0
@@ -35,8 +38,23 @@ CREATE TABLE Laituri (
 CREATE TABLE Metro (
     id INT PRIMARY KEY AUTO_INCREMENT,
     pvm DATE DEFAULT CURRENT_DATE,
+    kello TIME DEFAULT CURRENT_TIME,
     Asiak_palveltu INT DEFAULT 0,
     Keskim_jonotusaika FLOAT(5,2) DEFAULT 0,
     Simulointiaika FLOAT(10,2) DEFAULT 0
 );
+
+CREATE TABLE Asiakas (
+    id INT PRIMARY KEY NOT NULL,
+    pvm DATE DEFAULT CURRENT_DATE,
+    kello TIME DEFAULT CURRENT_TIME,
+    Asema_saap FLOAT DEFAULT 0,
+    Asema_pois FLOAT DEFAULT 0,
+    Lippu_saap FLOAT DEFAULT 0,
+    Lippu_pois FLOAT DEFAULT 0,
+    Laituri_saap FLOAT DEFAULT 0,
+    Laituri_pois FLOAT DEFAULT 0,
+    Metro_saap FLOAT DEFAULT 0,
+    Metro_pois FLOAT DEFAULT 0
+)
 
